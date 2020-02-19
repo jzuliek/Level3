6db879e547a571842f4db97dfc99d45c14723d42
Johnny Uribe <coconut@Johnnys-MacBook-Pro.local>
2020-02-19 15:25:54 -0800

    reorder

--- Notes/sideMenuEx/src/app/folder/folder.page.ts
+++ Notes/sideMenuEx/src/app/folder/folder.page.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { IonReorderGroup } from '@ionic/angular';
 
 @Component({
   selector: 'app-folder',
@@ -7,9 +8,25 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./folder.page.scss'],
 })
 export class FolderPage implements OnInit {
+  @ViewChild(IonReorderGroup, {static: false}) reorderGroup: IonReorderGroup;
+
   public folder: string;
 
   constructor(private activatedRoute: ActivatedRoute) { }
+  doReorder(ev: any) {
+    // The `from` and `to` properties contain the index of the item
+    // when the drag started and ended, respectively
+    console.log('Dragged from index', ev.detail.from, 'to', ev.detail.to);
+
+    // Finish the reorder and position the item in the DOM based on
+    // where the gesture ended. This method can also be called directly
+    // by the reorder group
+    ev.detail.complete();
+  }
+
+  toggleReorderGroup() {
+    this.reorderGroup.disabled = !this.reorderGroup.disabled;
+  }
 
   ngOnInit() {
     this.folder = this.activatedRoute.snapshot.paramMap.get('id');
