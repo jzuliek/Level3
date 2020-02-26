dc1991bef1ca16cd480b00be1b80f3c4938054d5
Johnny Uribe <coconut@Johnnys-MacBook-Pro.local>
2020-02-26 15:18:01 -0800

    colors added

--- bart-api/src/app/interfaces/station-info.ts
+++
@@ -1,8 +0,0 @@
-export interface StationInfo {
-    name: string,
-    abbr: string,
-    address: string,
-    city: string,
-    county:string,
-    zipcode: string
-}
--- bart-api/src/app/services/data.service.ts
+++ bart-api/src/app/services/data.service.ts
@@ -2,21 +2,25 @@ import { Injectable, RootRenderer } from '@angular/core';
 import { Station } from '../interfaces/station';
 import { HttpClient } from '@angular/common/http';
 import { Departure } from '../interfaces/departure';
-import { empty } from 'rxjs';
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
+
   stationURL = "http://api.bart.gov/api/stn.aspx?cmd=stns&key=Z2R9-5KQ2-9SYT-DWE9&json=y";
+
   town:string;
   address:string;
   city:string; 
   county:string;
   zipcode:string;
+
   depart: Departure[] = [];
+
   stations: Station[] = [];
+
   departure: Departure[] = [];
 
   constructor(private http: HttpClient) { }
@@ -29,6 +33,8 @@ export class DataService {
     return this.http.get<any>(url);
   }
 
+
+
   getDestination(url) {
     this.depart.length = 0;
     this.getDepart(url).subscribe(
@@ -37,17 +43,21 @@ export class DataService {
           let info: Departure =
           {
             destination: d.destination,
+
             estimate: [],
           }
           for (const e of d.estimate) {
             info.estimate.push(e);
           }
+
           this.depart.push(info);
         }
       }
+
       );
       console.log(this.depart);
   }
+
 
   getStations() {
     this.stations.length = 0;
@@ -57,10 +67,15 @@ export class DataService {
           const info: Station =
           {
             name: s.name,
+
             abbr: s.abbr,
+
             address: s.address,
+
             city: s.city,
+
             county: s.county,
+            
             zipcode: s.zipcode
           };
           this.stations.push(info);
